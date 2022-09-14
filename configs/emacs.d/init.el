7fa1f1e07da3ad57f0801ccd325415374634316c
Marcell Lipusz <marcell.lipusz@visma.com>
2022-09-14 20:24:05 +0200

    Switch back to git-gutter

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -214,18 +214,12 @@
 
 (electric-pair-mode 1)
 
-(use-package git-gutter+
+(use-package git-gutter
   :ensure t
-  :hook (prog-mode . git-gutter+-mode)
+  :hook (prog-mode . git-gutter-mode)
   :config
-  (setq git-gutter:update-interval 0.02))
-
-(use-package git-gutter-fringe+
-  :ensure t)
-
-(define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-(define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-(define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+  (setq
+   git-gutter:update-interval 0.02))
 
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers-type 'relative)
