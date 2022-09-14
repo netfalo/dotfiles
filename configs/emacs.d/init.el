b4ed380d2812953e3b4e428111d41626890568e9
Marcell Lipusz <marcell.lipusz@visma.com>
2022-09-14 16:54:55 +0200

    Add gitgutter to show modified rows

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -213,5 +213,18 @@
 
 (electric-pair-mode 1)
 
+(use-package git-gutter+
+  :ensure t
+  :hook (prog-mode . git-gutter+-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe+
+  :ensure t)
+
+(define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+(define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+(define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+
 (provide 'init)
 ;;; init.el ends here
