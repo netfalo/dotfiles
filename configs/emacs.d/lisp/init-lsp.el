fffdf350c38d9cedc2effd5df6730889a7fcf6f3
Marcell Lipusz <marcell.lipusz@visma.com>
2022-09-14 20:30:14 +0200

    Disable no language server warning (comes for emacs-lisp-mode)

--- configs/emacs.d/lisp/init-lsp.el
+++ configs/emacs.d/lisp/init-lsp.el
@@ -8,6 +8,7 @@
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-warn-no-matched-clients nil)
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (prog-mode . lsp)
          ;; if you want which-key integration
