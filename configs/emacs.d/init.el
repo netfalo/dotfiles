9fd39304ef097690cb413b8c7e83395d5a2d53d1
Marcell Lipusz <marcell.lipusz@visma.com>
2022-09-29 18:21:03 +0200

    Add functions to uniqify lines

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -243,5 +243,18 @@
 
 (load-theme 'zenburn)
 
+(defun uniq-region-lines (beg end)
+  "Remove duplicate adjacent lines in region."
+  (interactive "*r")
+  (save-excursion
+    (goto-char beg)
+    (while (re-search-forward "^\\(.*\n\\)\\1+" end t)
+      (replace-match "\\1"))))
+
+(defun uniq-buffer-lines ()
+  "Remove duplicate adjacent lines in the current buffer."
+  (interactive)
+  (uniquify-region-lines (point-min) (point-max)))
+
 (provide 'init)
 ;;; init.el ends here
